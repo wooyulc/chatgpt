49b2e50672148e2295a1b8b92bfe27cd45b0cd23
Wooyul Chung <wooyulc@uci.edu>
2023-03-02 04:24:18 -0800

    second

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
+
 import './App.css';
+import './normal.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <aside className="sidemenu">
+        <div className="side-menu-button">
+          <span>+</span>
+          New Chat
+        </div>
+      </aside>
+      <section className="chatbox">
+      <div className="chat-log">
+        <div className="chat-message">
+          <div className="avatar">
+            ME
+          </div>
+          <div className="message">
+            HELLO
+          </div>
+        </div>
+      </div>
+        <div className="chat-input-holder">
+          <textarea 
+            rows="1"
+            className="chat-input-textarea"
+          ></textarea>
+        </div>
+      </section>
+    
+      
     </div>
   );
 }
